8da6468a6ae8e3a1fe95af3e14eaf29e9faf659a
Haider Majid <92584858+HaiderSoftWare@users.noreply.github.com>
2024-06-20 14:17:16 +0300

    #employerByIdDB

--- Program.cs
+++ Program.cs
@@ -16,36 +16,12 @@ builder.Services.AddSingleton<List<EmploerModel>>(); // Register a singleton lis
 builder.Services.AddDbContext<EmployerDb>(options =>
     options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
 
-// Function to check database connection
-bool IsConnectionWorking(string connectionString)
-{
-    try
-    {
-        using (var connection = new NpgsqlConnection(connectionString))
-        {
-            connection.Open();
-            Console.WriteLine($"The connection is successful.");
-            return true;
-        }
-    }
-    catch (Exception ex)
-    {
-        // Log exception details here if necessary
-        Console.WriteLine($"Connection failed: {ex.Message}");
-        return false;
-    }
-}
-
-// Check the database connection
-var connectionString = configuration.GetConnectionString("DefaultConnection");
-if (!IsConnectionWorking(connectionString!))
-{
-    throw new Exception("Database connection failed. Please check the connection string and database server.");
-}
 
 var app = builder.Build();
 app.UseHttpsRedirection();
 app.MapEmployerEndpoints(app.Services.GetRequiredService<List<EmploerModel>>());
+
+
 
 
 
